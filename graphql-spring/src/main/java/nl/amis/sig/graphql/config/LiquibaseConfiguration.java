4d66ba852c394889a053e18dd0432d92e5c38405
chiel_h <chiel.ham@amis.nl>
2019-02-16 13:28:41 +0100

    liquibase config

---
+++ graphql-spring/src/main/java/nl/amis/sig/graphql/config/LiquibaseConfiguration.java
@@ -0,0 +1,19 @@
+package nl.amis.sig.graphql.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+@Configuration
+public class LiquibaseConfiguration {
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:config/liquibase/db.changelog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+}
